59c85f9264291a1d7db3bc6fded4d2f2ffb7069e
Yuliia Boiko <littlehappinies@gmail.com>
2022-07-31 14:26:32 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,13 +33,13 @@ export const App = () => {
       </Header>
       <Suspense fallback={<PageLoader />}>
         <Routes>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
+          <Route path="/goit-react-hw-05-movies/" element={<Home />} />
+          <Route path="/goit-react-hw-05-movies//movies" element={<Movies />} />
          <Route path=":movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="/goit-react-hw-05-movies//movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>  
